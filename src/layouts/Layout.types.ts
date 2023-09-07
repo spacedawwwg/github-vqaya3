f75146808f94320a5096ae3cfc627394c25712c2
spacedawwwg <spacedawwwg@gmail.com>
2023-09-07 14:09:32 +0100

    Initial commit

---
+++ src/layouts/Layout.types.ts
@@ -0,0 +1,6 @@
+import type { Context } from "../types";
+
+export interface LayoutProps {
+	title: string;
+	context?: Context;
+}
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export interface Context {
+    foo: string;
+    bar: number;
+}
