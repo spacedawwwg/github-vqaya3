dfe6eb578084c67dcee2a97ed44bce1cc2c60069
spacedawwwg <spacedawwwg@gmail.com>
2023-09-15 18:31:58 +0100

    3 files modified

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -9,11 +9,9 @@ defineProps<Props>();
 </script>
 
 <template>
-    <template v-for="item of items">
-      <div
-        v-if="$slots && $slots[item.id]"
-      >
-        <slot :name="item.id" />
-      </div>
-    </template>
+  <template v-for="item of items">
+    <div v-if="$slots && $slots[item.id]">
+      <slot :name="item.id" />
+    </div>
+  </template>
 </template>
