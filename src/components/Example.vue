44035c501fe64ac8e39f37771ca735c889afb676
Paul Welsh <spacedawwwg@gmail.com>
2023-09-15 15:17:05 +0000

    add example

---
+++ src/components/Example.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+export interface Props {
+  items: {
+    id: string;
+  }[];
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <template v-for="item of items">
+      <div
+        v-if="$slots && $slots[item.id]"
+      >
+        <slot :name="item.id" />
+      </div>
+    </template>
+</template>
